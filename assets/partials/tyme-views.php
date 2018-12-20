8ae269fcd6f2847851c5885de39df9f0082656d5
Tyler Bailey <tyler@elexicon.com>
2018-12-20 08:57:34 -0500

    Add markup to be rendered in the category metabox

--- assets/partials/tyme-views.php
+++ assets/partials/tyme-views.php
@@ -1,1 +1,11 @@
+<script type="text/html" id="tmpl-tyme-set-admin-views">
+  <a href="#" class="tyme-set-primary tyme-category">
+    <?php _e( 'Set Primary', 'tyme' ); ?>
+  </a>
+</script>
+<script type="text/html" id="tmpl-tyme-unset-admin-views">
+  <a href="#" class="tyme-unset-primary tyme-category">
+    <?php _e( 'Unset Primary', 'tyme' ); ?>
+  </a>
+</script>
 <?php wp_nonce_field( 'tyme_primary_nonce', 'tyme_primary_nonce' ); ?>
