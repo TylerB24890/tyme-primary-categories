356de50a11cc1ca778ab1d5dbc638e87e6e2c207
Tyler Bailey <tyler@elexicon.com>
2018-12-20 12:30:36 -0500

    Add blank index.php files for extra security. Misc. style updates, documentation changes & formatting

--- includes/classes/class-tyme-taxonomies.php
+++ includes/classes/class-tyme-taxonomies.php
@@ -85,4 +85,5 @@ class Tyme_Taxonomies {
       }
     }
   }
+  
 }
--- tyme-primary-category.php
+++ tyme-primary-category.php
@@ -11,24 +11,6 @@
  * License:     GPL-2.0+
  */
 
-/**
- * Copyright (c) 2018 Tyler Bailey (email : tylerb.media@gmail.com)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2 or, at
- * your discretion, any later version, as published by the Free
- * Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
  die( "Sneaky sneaky..." );
